779a1dc3fe457825adc50ff64b8a4e9b6b91b925
Anton <a.trubianov@gastrofix.com>
2016-10-11 15:19:14 +0300

    Create project

---
+++ AgileEngine/AgileEngine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AgileEngine
+//
+//  Created by Anton on 10/11/16.
+//  Copyright © 2016 AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AgileEngine/AgileEngine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AgileEngine
+//
+//  Created by Anton on 10/11/16.
+//  Copyright © 2016 AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
