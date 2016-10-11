5d1b5145b0228bee1ac870810bc8279da31b5aa7
Anton <a.trubianov@gastrofix.com>
2016-10-11 16:16:54 +0300

    flickr info

---
+++ AgileEngine/AgileEngine/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  AgileEngine
+//
+//  Created by Anton on 10/11/16.
+//  Copyright © 2016 AT. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+static NSString *const kFlickrKey = @"9cacc9cee6a4d7a8c328bb54aace47ac"
+static NSString *const kFlickrSecret = @"9ec15c81d9c819fb"
+
+#endif /* Constants_h */
